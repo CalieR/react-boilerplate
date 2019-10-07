6aafc5985a2b436ce4c950929eca065ce5720fc8
CalieR <calielrushton@gmail.com>
2019-10-07 12:54:39 +0100

    added error boundary and Strict mode

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import App from './App'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
+
+// React.StrictMode will warn if any deprecated lifecycle methods are being used
+
+// DefaultErrorBoundary will catch any exception thrown in the component tree, rendering an alternate UI so users aren't left with a blank screen
